d8f0efed2ebeeee8c3038f4dbdfc37e8e4977869
khalidxx <fxtryio@gmail.com>
2019-07-04 23:28:19 +0800

    basic R

---
+++ test1.R
@@ -0,0 +1,17 @@
+numA = 10
+numB <- 66
+numC = 6
+
+numDevil = numA * numB + numC
+
+print(numDevil)
+
+print(charToRaw('a'))
+
+print(is.integer(-11))
+
+print(is.numeric(11))
+
+print(is.integer(as.integer(10)))
+
+sprintf('(%d * %d) + %d = %3.2f!!!', numA, numB, numC, numDevil)
